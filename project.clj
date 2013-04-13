3216ac5419179635814d2104a28781c2901a7c17
Ruediger Gad <r.c.g@gmx.de>
2013-04-13 11:51:48 +0200

    Use latest clj-assorted-utils.

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
                 around jNetPcap that enables and eases packet capturing with Clojure."
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [org.clojure/tools.cli "0.2.2"]
-                 [clj-assorted-utils "1.0.0"]
+                 [clj-assorted-utils "1.4.0"]
                  [jnetpcap "1.4.r1390-1b"]]
   :main clj-net-pcap.main
   :java-source-paths ["src-java"]

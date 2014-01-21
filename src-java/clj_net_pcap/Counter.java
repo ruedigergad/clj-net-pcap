20bdad72194d64b451a7a2e80e29912db50bb8ae
Ruediger Gad <rgad@fb2.fh-frankfurt.de>
2014-01-21 15:01:37 +0100

    Add simple measures for thread safety.

--- src-java/clj_net_pcap/Counter.java
+++ src-java/clj_net_pcap/Counter.java
@@ -1,5 +1,8 @@
 package clj_net_pcap;
 
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
 /**
  *  This is an experiment.
  *  The primary purpose is to compare this to the Clojure counter implementation.
@@ -7,17 +10,38 @@ package clj_net_pcap;
 public class Counter {
 
     private long val = 0;
+    private Lock lock = new ReentrantLock();
 
     public void inc() {
-        val++;
+        /*
+         * In the current use case we prefer to miss an increment in favor of performance.
+         */
+        if (lock.tryLock()) {
+            val++;
+            lock.unlock();
+        }
     }
 
     public void reset() {
+        /*
+         * A reset has to allways succeed.
+         */
+        lock.lock();
         val = 0;
+        lock.unlock();
     }
 
     public long value() {
-        return val;
+        /*
+         * We prefer to return an invalid value in favor of performance.
+         * Invalid values are handled in the calling code.
+         */
+        long ret = -1;
+        if (lock.tryLock()) {
+            ret = val;
+            lock.unlock();
+        }
+        return ret;
     }
 
 }

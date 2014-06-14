f9d58cb44fbf225160d66c4ac01622fc61e75bab
Ruediger Gad <r.c.g@gmx.de>
2014-06-14 11:38:34 +0200

    Update version info.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clj-net-pcap "1.6.996"
+(defproject clj-net-pcap "1.6.997"
 ;(defproject clj-net-pcap "1.7.0-SNAPSHOT"
   :description "clj-net-pcap is a wrapper/adapter/facade (No matter how you wanna call it.) 
                   around jNetPcap that enables and eases packet capturing with Clojure."

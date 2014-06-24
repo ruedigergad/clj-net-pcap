dc8eac915814e9b49c8d51acc83f66c9db161f66
Ruediger Gad <r.c.g@gmx.de>
2014-06-24 19:20:15 +0200

    Try if coveralls failed due to wrong permissions.

--- test/coveralls.sh
+++ test/coveralls.sh
@@ -1,4 +1,6 @@
 COVERALLS_URL='https://coveralls.io/api/v1/jobs'
 sudo LEIN_ROOT=1 lein2 cloverage -o cov --coveralls
+sudo chmod 755 cov
+sudo chmod 644 cov/coveralls.json
 curl -F 'json_file=@cov/coveralls.json' "$COVERALLS_URL"
 

f58bcf704928d971d3c52d34aacc9f93686a0d31
Ruediger Gad <r.c.g@gmx.de>
2015-01-01 20:31:19 +0100

    Use latest clj-assorted-utils and get-with-default.

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
                 around jNetPcap that enables and eases packet capturing and processing with Clojure."
   :dependencies [[org.clojure/clojure "1.6.0"]
                   [org.clojure/tools.cli "0.2.4"]
-                  [clj-assorted-utils "1.7.0"]
+                  [clj-assorted-utils "1.8.0"]
                   [jnetpcap "1.4.r1425-1d"]]
   :min-lein-version "2.0.0"
   :aot [#"^clj-net-pcap.*"]

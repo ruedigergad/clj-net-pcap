74219d1e0e06b84d6695f106f6e6b14025b57a23
Ruediger Gad <rgad@fb2.fh-frankfurt.de>
2015-01-27 12:00:41 +0100

    Add packet-byte-array-extract-map-ipv4-udp to help.

--- src/clj_net_pcap/main.clj
+++ src/clj_net_pcap/main.clj
@@ -81,7 +81,8 @@
      (str "Use the specified function for transforming the raw packets."
           " Available function names are:\n"
           "pcap-packet-to-bean, pcap-packet-to-map, pcap-packet-to-nested-maps,\n"
-          "pcap-packet-to-bean-ipv4-udp-only, pcap-packet-to-map-ipv4-udp-only, no-op")
+          "pcap-packet-to-bean-ipv4-udp-only, pcap-packet-to-map-ipv4-udp-only,\n"
+          "packet-byte-array-extract-map-ipv4-udp, no-op")
      :default "pcap-packet-to-bean"]
     ["-R" "--read-file"
      "Read from a pcap file instead of performing a live capture."

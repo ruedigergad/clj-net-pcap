93d1449c45ef29d113e9c258708c18940fe9f7f6
Ruediger Gad <r.c.g@gmx.de>
2015-01-31 20:32:50 +0100

    Add namespaces for self-adaptivity foo.

---
+++ src/clj_net_pcap/self_adaptive_dsl_adjustment.clj
@@ -0,0 +1,30 @@
+;;;
+;;; Copyright (C) 2015 Ruediger Gad
+;;;
+;;; This file is part of clj-net-pcap.
+;;;
+;;; clj-net-pcap is free software: you can redistribute it and/or modify
+;;; it under the terms of the GNU Lesser General Public License (LGPL) as
+;;; published by the Free Software Foundation, either version 3 of the License,
+;;; or (at your option any later version.
+;;;
+;;; clj-net-pcap is distributed in the hope that it will be useful,
+;;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;;; GNU Lesser General Public License (LGPL) for more details.
+;;;
+;;; You should have received a copy of the GNU Lesser General Public License (LGPL)
+;;; along with clj-net-pcap.  If not, see <http://www.gnu.org/licenses/>.
+;;;
+
+(ns
+  ^{:author "Ruediger Gad",
+    :doc "A simple proof of concept for adjusting DSL statements with self-adaptivity."}
+  clj-net-pcap.self-adaptive-dsl-adjustment
+  (:use clojure.pprint
+        clj-assorted-utils.util))
+
+(defn create-stat-delta-counter
+  []
+  (let []
+    (fn [current-stats])))
---
+++ test/clj_net_pcap/test/self_adaptive_dsl_adjustment.clj
@@ -0,0 +1,37 @@
+;;;
+;;; Copyright (C) 2015 Ruediger Gad
+;;;
+;;; This file is part of clj-net-pcap.
+;;;
+;;; clj-net-pcap is free software: you can redistribute it and/or modify
+;;; it under the terms of the GNU Lesser General Public License (LGPL) as
+;;; published by the Free Software Foundation, either version 3 of the License,
+;;; or (at your option any later version.
+;;;
+;;; clj-net-pcap is distributed in the hope that it will be useful,
+;;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;;; GNU Lesser General Public License (LGPL) for more details.
+;;;
+;;; You should have received a copy of the GNU Lesser General Public License (LGPL)
+;;; along with clj-net-pcap.  If not, see <http://www.gnu.org/licenses/>.
+;;;
+
+(ns 
+  ^{:author "Ruediger Gad",
+    :doc "Tests for applying DSL expressions with self-adaptivity."}
+  clj-net-pcap.test.self-adaptive-dsl-adjustment
+  (:use clojure.test
+        clj-net-pcap.self-adaptive-dsl-adjustment
+        clj-assorted-utils.util)
+  (:import (clj_net_pcap PacketHeaderDataBeanIpv4UdpOnly)))
+
+(deftest stat-delta-test
+  (let [stat-1 {"forwarder-failed" 1, "out-dropped" 2, "out-queued" 3, "recv" 4, "drop" 5, "ifdrop" 6}
+        stat-2 {"forwarder-failed" 6, "out-dropped" 5, "out-queued" 4, "recv" 5, "drop" 6, "ifdrop" 7}
+        expected-delta-1 {"forwarder-failed" 1, "out-dropped" 2, "out-queued" 3, "recv" 4, "drop" 5, "ifdrop" 6}
+        expected-delta-2 {"forwarder-failed" 5, "out-dropped" 3, "out-queued" 1, "recv" 1, "drop" 1, "ifdrop" 1}
+        stat-d-cntr (create-stat-delta-counter)]
+    (is (= expected-delta-1 (stat-d-cntr stat-1)))
+    (is (= expected-delta-2 (stat-d-cntr stat-2)))))
+

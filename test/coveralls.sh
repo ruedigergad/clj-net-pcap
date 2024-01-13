623d963f00ba2515b5e9f7c6b22b2451b4931220
Ruediger Gad <r.c.g@gmx.de>
2024-01-13 20:08:03 +0100

    Coverage results are not pushed via CircleCI coveralls orb.

--- test/coveralls.sh
+++
@@ -1,8 +0,0 @@
-COVERALLS_URL='https://coveralls.io/api/v1/jobs'
-TRAVIS_JOB_ID_TMP=$TRAVIS_JOB_ID
-echo "Job id: $TRAVIS_JOB_ID_TMP"
-sudo LEIN_ROOT=1 TRAVIS=true TRAVIS_JOB_ID=$TRAVIS_JOB_ID_TMP lein2 cloverage -o cov --coveralls
-sudo chmod 755 cov
-sudo chmod 644 cov/coveralls.json
-curl -F 'json_file=@cov/coveralls.json' "$COVERALLS_URL"
-
